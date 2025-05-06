1bdcd691ba40db9d75bad13b417b4cfe1038a164
Do Khanh Khiem Tu <kitudo@students.cs.ubc.ca>
2025-05-06 15:36:54 -0700

    edit

--- index.js
+++ index.js
@@ -4,32 +4,6 @@ const url = require('url');
 const slugify = require('slugify');
 const replaceTemplate = require('./modules/replaceTemplate');
 
-////////
-/// FILES
-
-// Blocking, synchronous way
-// const text1 = fs.readFileSync('./starter/txt/input.txt', 'utf-8');
-// console.log(text1);
-// const textOut = `This is what we know about the avocado: ${text1}.\nCreate on ${Date.now()}`;
-// fs.writeFileSync('./starter/txt/output.txt', textOut);
-// console.log('File Written');
-
-// Non-blocking, asynchronous way
-// fs.readFile('./starter/txt/start.txt', 'utf-8', (err, data1) => {
-//     if (err) return console.log('ERROR!!!');
-
-//     fs.readFile(`./starter/txt/${data1}.txt`, 'utf-8', (err, data2) => {
-//         console.log(data2);
-//         fs.readFile('./starter/txt/append.txt', 'utf-8', (err, data3) => {
-//             console.log(data3);
-
-//             fs.writeFile('./starter/txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
-//                 console.log('Your file has been written! 😄');
-//             });
-//         });
-//     });
-// });
-// console.log('Will read file!');
 
 ////////
 /// SERVER
